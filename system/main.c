e926ea9fdf083130c25a62de732ebe575967f4a7
Anzhen Zhang <benadette.chang@gmail.com>
2012-07-01 07:40:33 +0800

    system

---
+++ system/main.c
@@ -0,0 +1,48 @@
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h> 
+#include <unistd.h>
+#include "gloable.h"
+
+int main() {
+//    char trace_data[1024];
+    struct trace *trace = (struct trace*)malloc(sizeof(struct trace)); 
+//    struct node *tail = NULL;
+ 
+    get_trace(trace);
+    struct node *p1 = trace->head;
+    struct node *p2 = trace->head->next;
+    //在压缩之前的trace内容如下
+    printf("before delete the similar nodes:\n");
+    while (p1) {
+        printf("(%c , %c) ", p1->x, p1->y);
+        p1 = p1->next;
+    }
+    p1 = trace->head;
+    // 处理冗余数据，压缩存储
+    while (p2) {
+        if( (((int)(p2->x-'0') -(int) (p1->x-'0'))*((int)(p2->x-'0') -(int) (p1->x-'0')) + ((int)(p2->y-'0') - (int)(p1->y-'0'))*((int)(p2->y-'0') - (int)(p1->y-'0'))) < 3) {
+            p1->next = p2->next;
+            p2 = p1->next;
+        } else {
+            p1 = p2;
+            p2 = p2->next;
+        }
+    }
+    //打印压缩后的trace内容
+    printf("\nafter process \n");
+    p1 = trace->head;
+    while(p1){
+        printf("(%c , %c) ", p1->x, p1->y);
+        p1 = p1->next;
+    }
+    printf("\n");
+
+    p1 = trace->head;
+    while(p1){
+	p2 = p1;
+	p1 = p1->next;
+	free(p2);
+    }
+    return 0;
+}
---
+++ system/new_trace_file.c
@@ -0,0 +1,38 @@
+#include <sys/stat.h>
+#include<stdio.h>
+#include<time.h>
+#include<sys/types.h>
+
+#include "gloable.h"
+
+int main () {
+    //获取当前时间的函数
+    time_t nowtime;
+    struct tm *timeinfo;
+    time(&nowtime);
+    timeinfo=localtime(&nowtime);
+    char *txtname = NULL;
+    txtname = asctime(timeinfo);//结果转变为字符串
+
+    mkdirectory();
+    int i;
+    for(i=0;txtname[i]!='\0';i++)
+    {
+       int j=7+i;
+       pathname[j]=txtname[i];
+    }
+    pathname[i+7]='.'; //加上后缀 .txt的
+    pathname[i+8]='t';
+    pathname[i+9]='x';
+    pathname[i+10]='t';
+    pathname[i+11]='\0';//最后别忘记加上这个
+
+    FILE *fp;
+    if((fp=fopen(pathname,"w+"))==NULL)//打开文件 没有就创建
+    {
+        printf("can not creat file!\n");
+    }
+
+    fprintf(fp,"hello the first created txt! successed!!!");
+    fclose(fp);
+}
